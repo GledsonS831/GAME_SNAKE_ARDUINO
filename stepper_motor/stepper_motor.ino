23006dacaa56f624902ce7541423e7f1fb362f33
GLEDSONS831 <glegogle84@gmail.com>
2019-10-16 12:53:16 -0300

    change code

--- stepper_motor/stepper_motor.ino
+++ stepper_motor/stepper_motor.ino
@@ -1,27 +1,29 @@
 #include <Stepper.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-// for your motor
+// change this to the number of steps on your motor
+#define STEPS 100
 
+// create an instance of the stepper class, specifying
+// the number of steps of the motor and the pins it's
+// attached to
+Stepper stepper(STEPS, 8, 9, 10, 11);
 
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-
-int stepCount = 0;  // number of steps the motor has taken
+// the previous reading from the analog input
+int previous = 0;
 
 void setup() {
-  // nothing to do inside the setup
+  // set the speed of the motor to 30 RPMs
+  stepper.setSpeed(30);
 }
 
 void loop() {
-  // read the sensor value:
-  int sensorReading = analogRead(A0);
-  // map it to a range from 0 to 100:
-  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
-  // set the motor speed:
-  if (motorSpeed > 0) {
-    myStepper.setSpeed(motorSpeed);
-    // step 1/100 of a revolution:
-    myStepper.step(stepsPerRevolution / 100);
-  }
+  // get the sensor value
+  int val = analogRead(0);
+
+  // move a number of steps equal to the change in the
+  // sensor reading
+  stepper.step(val - previous);
+
+  // remember the previous value of the sensor
+  previous = val;
 }
