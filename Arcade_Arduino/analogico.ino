6a33cacdcf40dd47ed69ac8cd883513e8ea8dbd1
AYRtonMeD <48737985+AYRtonMeD@users.noreply.github.com>
2019-11-21 10:04:57 -0300

    Update analogico.ino

--- Arcade_Arduino/analogico.ino
+++ Arcade_Arduino/analogico.ino
@@ -1,16 +1,18 @@
-int *ler_analogico_x (){
-  int* x = (int*)malloc(sizeof(int));
-  *x = analogRead(A0);
+int ler_analogico_x (){
+  int x;
+  x = analogRead(A1);
   return x;
 }
 
-int *ler_analogico_y (){
-  int* y = (int*)malloc(sizeof(int));
-  *y = analogRead(A1);
+int ler_analogico_y (){
+  int y;
+  y = analogRead(A0);
   return y;
 }
 
-void funcao (int* p1, int* p2, int backup_p1, int backup_p2, int tempo, bool manter_anterior){
+void funcao (int* p1, int* p2, int backup_p1, int backup_p2, bool manter_anterior){
+  int tempo = 100;
+  
   if (*p1 > tempo){
     *p1 = 1;
     *p2 = 0;
@@ -32,29 +34,29 @@ void funcao (int* p1, int* p2, int backup_p1, int backup_p2, int tempo, bool man
 void ler_analogico (int* x, int* y, bool vertical, bool horizontal, bool manter_anterior){
   int backup_x = *x;
   int backup_y = *y;
-  int tempo = 100;
-  
-  if (vertical && horizontal){
-    x = ler_analogico_x();
+
+    *x = ler_analogico_x();
     *x -= 512;
-    y = ler_analogico_y();
+    *y = ler_analogico_y();
     *y -= 512;
 
+  if (vertical && horizontal){
+
     if (abs(*y) >= abs(*x)){
-      funcao (y, x, backup_y, backup_x, tempo, manter_anterior);
+      funcao (y, x, backup_y, backup_x, manter_anterior);
     }
-    
+
     else {
-      funcao (x, y, backup_x, backup_y, tempo, manter_anterior);
+      funcao (x, y, backup_x, backup_y, manter_anterior);
     }
   }
   else if (vertical){
     *x = 0;
-    funcao (y, x, backup_y, backup_x, tempo, manter_anterior);
+    funcao (y, x, backup_y, backup_x, manter_anterior);
   }
   else {
-    x = ler_analogico_x();
-    funcao (x, y, backup_x, backup_y, tempo, manter_anterior);
+    *y = 0;
+    funcao (x, y, backup_x, backup_y, manter_anterior);
   }
                        Serial.print(*x);
   Serial.print(" ");
@@ -66,9 +68,13 @@ void setup() {
   Serial.begin(9600);
 }
 void loop(){
-  int *x;
-  int *y;
-  ler_analogico(x, y, true, true, true);
-
+  int *x = (int*)malloc(sizeof(int));
+  *x = 0;
+  int *y = (int*)malloc(sizeof(int));
+  *y = 0;
+  ler_analogico(x, y, true, true, false);
+Serial.print(*x);
+Serial.print(" ");
+Serial.println(*y);
   delay(2000);
 }
