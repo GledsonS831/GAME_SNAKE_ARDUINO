5e16750a0ded85aa1db05cd8cdbe42a17f165760
AYRtonMeD <48737985+AYRtonMeD@users.noreply.github.com>
2019-12-05 16:49:58 -0300

    Update Escape_game.ino

--- Arcade_Arduino/Escape_game/Escape_game.ino
+++ Arcade_Arduino/Escape_game/Escape_game.ino
@@ -1,8 +1,81 @@
 #include <LedControl.h>
 
 LedControl lc = LedControl(12, 11, 10, 4);
-int rdz = 0;
-bool trap[8][8];
+
+bool campo[8][8];
+
+struct nave{
+  byte velocidade_tiro;
+  byte velocidade_reload;
+};
+
+struct nave* init_nave(){
+  struct nave* nave = (struct nave*)malloc(sizeof(struct nave));
+  nave->velocidade_tiro = 1000;
+  nave->velocidade_reload = 1500;
+  return nave;
+}
+
+struct Node{
+  byte pos_x;
+  byte pos_y;
+  struct Node *node_anterior;
+  struct Node *node_proximo;
+};
+struct linked_list{
+  byte lenght;
+  struct Node* node;
+  struct Node* last_node;
+};
+
+struct linked_list* create_linked_list(){
+  struct linked_list* l = (struct linked_list*)malloc (sizeof(struct linked_list));
+  l->lenght = 0;
+  l->node = NULL;
+  l->last_node = NULL;
+}
+
+void list_put_element(struct linked_list* l, byte x, byte y){
+  struct Node* node = (struct Node*) malloc (sizeof(struct Node));
+  node->pos_x = x;
+  node->pos_y = y;
+  node-> node_proximo = NULL;
+  node->node_anterior = NULL;
+
+  if (l->node == NULL){
+    l->node = node;
+    l->last_node = node;
+  }
+  else{
+    node->node_proximo = l->node;
+    node->node_anterior = l->last_node;
+    l->last_node->node_proximo = node;
+  }
+}
+
+void list_remove_element (struct linked_list* l, struct Node* node){
+    if (node->node_proximo == NULL){
+      l->node = NULL;
+      l->last_node = NULL;
+    }
+    else{
+      node->node_anterior->node_proximo = node->node_proximo;
+    }
+
+    free(node);
+}
+
+void reset (){
+  asm volatile (" jmp 0");
+}
+
+void limpar_campo(){
+  for(byte i = 0; i < 8; i++){
+    for(byte j = 0; j < 8; j++){
+      campo[i][j] = 0;
+    }
+  }
+}
 void setup() {
   lc.shutdown(0, false);
   lc.shutdown(1, false);
@@ -13,85 +86,31 @@ void setup() {
   lc.setIntensity(1,0);
   lc.setIntensity(2,0);
   lc.setIntensity(3,0);
+  randomSeed(analogRead(5));
+  for (int i = 0; i < 8; i++){
+    lc.setRow(3, i, B11111111);
+  }
+  lc.setRow(2, 0, B11111100);
+  lc.setRow(2, 1, B11111000);
+  lc.setRow(2, 2, B11111110);
+  lc.setRow(2, 3, B11110000);
+  lc.setRow(2, 4, B11110000);
+  lc.setRow(2, 5, B11111110);
+  lc.setRow(2, 6, B11111000);
+  lc.setRow(2, 7, B11111100);
 
   Serial.begin(9600);
 }
 
-void seleciona_pos(){
-  rdz = random(0, 4);
-  trap[0][rdz] = 1;
-  rdz = random(4, 8);
-  trap[7][rdz] = 1;
-
-
-  rdz = random(1, 4);
-  trap[rdz][0] = 1;
-  rdz = random(4, 8);
-  trap[rdz][7] = 1;
-}
-
-
-void acender_armadilha(){
-  seleciona_pos();
-  for(byte k = 0; k < 4; k++){
-    
-    for(byte i = 0; i < 8; i++){
-      lc.setLed(1, 0, i, trap[0][i]);
-      lc.setLed(1, i, 0, trap[i][0]);
-      lc.setLed(1, 7, i, trap[7][i]);
-    }
-    delay(500);
-    limpar_matriz_led();
-    delay(500);
+void atirar(byte pos){
+  for(byte i = 0; i < 8; i++){
+    lc.setLed(1, pos, i, true);
+    lc.setLed(1, pos, i, false);
+    delay(400);
   }
 }
 
-void limpar_matriz_game(){
-  for(byte i = 0; i < 8; i++){
-      for(byte j = 0; j < 8; j++){
-        trap[i][j] = 0;
-      }
-   }
-   limpar_matriz_led();
-}
-
-void limpar_matriz_led(){
-  
-  for(byte i = 0; i < 8; i++){
-      for(byte j = 0; j < 8; j++){
-        lc.setLed(1, i, j, false);
-      }
-   }
- 
-}
-
-void atirar(){
-  for(byte i = 0; i < 8; i++){
-      if(trap[0][i]){
-        for(byte j = 0; j < 8; j++){
-          lc.setLed(1, j, i, true);
-        }
-      }
-
-      if(trap[i][0]){
-        for(byte j = 0; j < 8; j++){
-          lc.setLed(1, i, j, true);
-        }
-      }
-      
-      if(trap[7][i]){
-        for(byte j = 0; j < 8; j++){
-          lc.setLed(1, j, i, true);
-        }
-      }
-   }
-   delay(500);
-}
 void loop() {
- acender_armadilha();
- delay(600);
- atirar();
- delay(1000);
- limpar_matriz_game();
- delay(2000);
+
+// asm volatile (" jmp 0");
 }
