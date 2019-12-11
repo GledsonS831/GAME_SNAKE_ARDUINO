5d332afb5b03b0c563f4d6da484fe332711358a3
AYRtonMeD <48737985+AYRtonMeD@users.noreply.github.com>
2019-12-11 16:55:52 -0300

    Update anaconda2.ino

--- Arcade_Arduino/anaconda2.ino
+++ Arcade_Arduino/anaconda2.ino
@@ -1,8 +1,11 @@
-//#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>
 #include "LedControl.h"
+#include <EEPROM.h>
 
 LedControl lc = LedControl(12, 11, 10, 4);
-//LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+
+volatile bool pause = false;
 
 struct anaconda{
   int pos_atual[3];
@@ -73,10 +76,44 @@ void ler_analogico (int* x, int* y, bool vertical, bool horizontal, bool manter_
 }
 
 void clearDisplay(){
-  lc.clearDisplay(0);
   lc.clearDisplay(1);
   lc.clearDisplay(2);
   lc.clearDisplay(3);
+}
+void menu(){
+  detachInterrupt(digitalPinToInterrupt(3));
+  pause = !pause;
+  delayMicroseconds(300000);
+  attachInterrupt(digitalPinToInterrupt(3), menu, FALLING);
+}
+
+void mostar_vida(int vida){
+  lc.clearDisplay(0);
+
+  if (vida == 3){
+    lc.setRow(0, 2, 0b11111110);
+    lc.setColumn(0, 0, 0b00111100);
+    lc.setColumn(0, 3, 0b00111100);
+    lc.setColumn(0, 6, 0b00111100);
+  }
+
+  else if (vida == 2){
+    lc.setRow(0, 2, 0b11110000);
+    lc.setRow(0, 5, 0b00011110);
+    lc.setColumn(0, 0, 0b00111100);
+    lc.setColumn(0, 3, 0b00111100);
+    lc.setColumn(0, 6, 0b00111100);
+  }
+
+  else if (vida == 1){
+    lc.setRow(0, 5, 0b11111110);
+  }
+  else{
+    lc.setColumn(0, 0, 0b00111100);
+    lc.setRow(0, 2, 0b11111110);
+    lc.setRow(0, 5, 0b11111110);
+    lc.setColumn(0, 6, 0b00111100);
+  }
 }
 
 void select_pos_food (struct anaconda* ana){
@@ -102,6 +139,32 @@ void blink_food (struct anaconda* ana){
     estado = !estado;
     delay (10);
   }
+}
+
+void atualiza_pontuacao (int val){
+  lcd.setCursor(11, 1);
+  lcd.print(val);
+
+  if (val > (EEPROM.read(1) << 8) + EEPROM.read(0)){
+    lcd.setCursor(2, 1);
+    lcd.print(val);
+  }
+}
+void init_lcd (){
+  lcd.setCursor(0,0);
+  lcd.print("ANACONDA'S GAME!");
+
+  lcd.setCursor(0,1);
+  lcd.print("R ");
+  lcd.print( (EEPROM.read(1) << 8) + EEPROM.read(0));
+
+  lcd.setCursor(9, 1);
+  lcd.print("P ");
+  lcd.print(0);
+}
+void reset_recorde(){
+  EEPROM.update(0, 0);
+  EEPROM.update(1, 0);
 }
 
 void andar_anaconda (int x, int y, int* pos_z, int* pos_x, int* pos_y){
@@ -142,7 +205,6 @@ void andar_bunda_anaconda (struct anaconda* ana){
     andar_anaconda (0, -1, &ana->pos_final[0], &ana->pos_final[1], &ana->pos_final[2]);
   }
 }
-
 void andar_cabeca_anaconda (int x, int y, struct anaconda* ana){
 
   if (x == 1 && y == 0) ana->mapeamento[ana->pos_atual[0]][ana->pos_atual[1]][ana->pos_atual[2]] = 1;
@@ -155,12 +217,14 @@ void andar_cabeca_anaconda (int x, int y, struct anaconda* ana){
   lc.setLed(ana->pos_atual[0], ana->pos_atual[1], ana->pos_atual[2], true);
 
 }
-
 bool colision_anaconda(int x, int y, struct anaconda* ana){//----------------------------------
   andar_cabeca_anaconda(x, y, ana);
   if (ana->mapeamento[ana->pos_atual[0]][ana->pos_atual[1]][ana->pos_atual[2]] == 5){
     ++ana->tamanho;
-    if (ana->pontos < 10000) ana->pontos += 100;
+    if (ana->pontos < 10000){
+      ana->pontos += 100;
+      atualiza_pontuacao(ana->pontos);
+    }
     select_pos_food(ana);
   }
   else{
@@ -172,10 +236,11 @@ bool colision_anaconda(int x, int y, struct anaconda* ana){//-------------------
   }
 
   blink_food(ana);
-  if (ana->nivel * 50 == 150) delay(50);
+  if (ana->nivel * 10 == 150) delay(50);
   else delay(200 - (ana->nivel * 10));
   return true;
 }
+
 void zerarMatriz (int matriz [4][8][8]){
   for (int i = 1; i < 4; i++){
     for (int j = 0; j < 8; j++){
@@ -202,7 +267,6 @@ void next_level_anaconda(struct anaconda* ana){
   ana->tamanho = 1;
   ++ana->nivel;
 }
-
 struct anaconda* init_anaconda(){
   struct anaconda* ana = (struct anaconda*)malloc(sizeof(struct anaconda));
 
@@ -216,19 +280,30 @@ void snake (){
   int y = 0;
   struct anaconda* ana = init_anaconda();
 
-  for (int i = 0; i < 3; i++){
+  for (int i = 3; i > 0; i--){
+    mostar_vida(i);
     do{
+      while(pause);
       if (ana->tamanho == 8){
         next_level_anaconda(ana);
         x = 0;
         y = 0;
+        delay(500);
       }
       ler_analogico(&x, &y, true, true, true);
-      Serial.println(ana->pontos);
+
     }while(colision_anaconda(x, y, ana));
     --ana->nivel;
     ana->tamanho = 8;
   }
+  mostar_vida(0);
+  if (ana->pontos > (EEPROM.read(1) << 8) + EEPROM.read(0)){
+    EEPROM.update(1, ana->pontos >> 8);
+    EEPROM.update(0, ana->pontos);
+  }
+  free(ana);
+
+  delay(2000);
 }
 
 void setup() {
@@ -238,32 +313,24 @@ void setup() {
   lc.shutdown(2,false);
   lc.shutdown(3,false);
 
-  // seta o brilho para um valor mediano:
-  //-------------------------------------
   lc.setIntensity(0,0);
   lc.setIntensity(1,0);
   lc.setIntensity(2,0);
   lc.setIntensity(3,0);
 
-  //lcd.begin (16,2);
-  //lcd.setBacklight(HIGH);
+  lcd.begin (16,2);
+  lcd.setBacklight(HIGH);
+  //reset_recorde();
+  init_lcd();
 
-  // limpa o display:
-  //-----------------
-  //clearDisplay();
+  pinMode(3, INPUT);
+  attachInterrupt(digitalPinToInterrupt(3), menu, RISING);
 
   Serial.begin(9600);
   randomSeed(analogRead(A3));
 }
+
 void loop(){
   snake();
-  /*
-  lcd.setCursor(0,0);
-  lcd.print("--nome do jogo--");
-  lcd.setCursor(0,1);
-  lcd.print("R -----");
-  lcd.setCursor(9, 1);
-  lcd.print("P -----");
-  */
-
+  //Serial.println(digitalRead(3));
 }
